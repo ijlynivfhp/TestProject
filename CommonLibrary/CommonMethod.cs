793bea486b7d38ff75bf82c0e6847ffa56330e4b
ijlynivfhp <380726211@qq.com>
2021-04-24 15:26:49 +0800

    0000

---
+++ CommonLibrary/CommonMethod.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary
+{
+    public partial class CommonMethod
+    {
+        #region 实体对象集合转成动态类Or集合
+        public static TNew DtoListToTOrTList<TOld,TNew>(TOld tOld) {
+            string str = JsonConvert.SerializeObject(tOld);
+            return JsonConvert.DeserializeObject<TNew>(str);
+        }
+        #endregion
+
+        #region 泛型集合转DataTable  
+        /// <summary>  
+        /// 泛型集合转DataTable  
+        /// </summary>  
+        /// <typeparam name="T">集合类型</typeparam>  
+        /// <param name="entityList">泛型集合</param>  
+        /// <returns>DataTable</returns>  
+        public static DataTable ListToDt<T>(IList<T> entityList)
+        {
+            if (entityList == null) return null;
+            DataTable dt = CreateTable<T>();
+            Type entityType = typeof(T);
+            //PropertyInfo[] properties = entityType.GetProperties();  
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entityType);
+            foreach (T item in entityList)
+            {
+                DataRow row = dt.NewRow();
+                foreach (PropertyDescriptor property in properties)
+                {
+                    row[property.Name] = property.GetValue(item);
+                }
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+        #endregion
+
+        #region 创建DataTable的结构  
+        private static DataTable CreateTable<T>()
+        {
+            Type entityType = typeof(T);
+            //PropertyInfo[] properties = entityType.GetProperties();  
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entityType);
+            //生成DataTable的结构  
+            DataTable dt = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+            {
+                dt.Columns.Add(prop.Name);
+            }
+            return dt;
+        }
+        #endregion
+
+    }
+}
---
+++ DTO/CommonDto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    /// <summary>
+    /// 共用测试对象
+    /// </summary>
+    public partial class CommonDto
+    {
+        public string FieldA { get; set; }
+        public string FieldB { get; set; }
+        public string FieldC { get; set; }
+        public string FieldD { get; set; }
+        public string FieldE { get; set; }
+        public string FieldF { get; set; }
+        public string FieldG { get; set; }
+        public string FieldH { get; set; }
+        public string FieldI { get; set; }
+        public string FieldJ { get; set; }
+        public string FieldK { get; set; }
+        public string FieldL { get; set; }
+        public string FieldM { get; set; }
+        public string FieldN { get; set; }
+        public string FieldO { get; set; }
+        public string FieldP { get; set; }
+        public string FieldQ { get; set; }
+        public string FieldR { get; set; }
+        public string FieldS { get; set; }
+        public string FieldT { get; set; }
+    }
+
+    /// <summary>
+    /// 备用扩展列数
+    /// </summary>
+    //public partial class CommonDto
+    //{
+    //    public string FieldAA { get; set; }
+    //    public string FieldBB { get; set; }
+    //    public string FieldCC { get; set; }
+    //    public string FieldDD { get; set; }
+    //    public string FieldEE { get; set; }
+    //    public string FieldFF { get; set; }
+    //    public string FieldGG { get; set; }
+    //    public string FieldHH { get; set; }
+    //    public string FieldII { get; set; }
+    //    public string FieldJJ { get; set; }
+    //    public string FieldKK { get; set; }
+    //    public string FieldLL { get; set; }
+    //    public string FieldMM { get; set; }
+    //    public string FieldNN { get; set; }
+    //    public string FieldOO { get; set; }
+    //    public string FieldPP { get; set; }
+    //    public string FieldQQ { get; set; }
+    //    public string FieldRR { get; set; }
+    //    public string FieldSS { get; set; }
+    //    public string FieldTT { get; set; }
+    //}
+}
